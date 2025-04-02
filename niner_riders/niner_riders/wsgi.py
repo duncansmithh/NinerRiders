bcf0797d39dff52dfbdb25089668a45b8a738117
Duncan Smith <smithduncan40@gmail.com>
2025-04-02 16:14:15 -0400

    Initial Django project setup

---
+++ niner_riders/niner_riders/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for niner_riders project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'niner_riders.settings')
+
+application = get_wsgi_application()
