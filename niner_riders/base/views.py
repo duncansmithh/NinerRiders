e0639f3d9d93038be1913a0cac557921f815fad5
Duncan Smith <smithduncan40@gmail.com>
2025-04-12 19:01:29 -0400

    Rooms

--- niner_riders/base/views.py
+++ niner_riders/base/views.py
@@ -4,10 +4,16 @@ from django.db.models import Q
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Need Rides'},
+    {'id': 2, 'name': 'Giving Rides'},
+]
+
 def home(request):
+    context = {'rooms': rooms}
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'base/room.html')
