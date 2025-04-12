623454dd6d727acc660de54dbf14257803d9bb61
[jgraeber2] <[jgraeber@uncc.edu]>
2025-04-12 15:20:05 -0400

    Add home template

--- niner_riders/base/views.py
+++ niner_riders/base/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome to the Niner Riders app!")
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    return render(request, 'base/home.html')
 
 def room(request):
     return HttpResponse("Room")
